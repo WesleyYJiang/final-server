10f8600096b137cba7a507a3953ebbeb32abe42a
wesleyjiang <jiang.yuto@husky.neu.edu>
2018-05-13 23:54:11 -0400

    added services, create user

--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -4,15 +4,19 @@
 	
     var tbody;
     var template;
+    var userService = new UserServiceClient()
 	
     function main() {
 		tbody = $('tbody');
         template = $('.template');
 		var promise = fetch("http://localhost:8080/api/user");
-		promise.then(function (response) {
-			return response.json();
-		}).then(renderUsers)
+		$('#createUser').click(createUser);
+		findAllUsers();
 	}
+    
+    function findAllUsers() {
+        userService.findAllUsers().then(renderUsers);
+    }
 	
     function renderUsers(users) {
         for(var i = 0; i < users.length; i++) {
@@ -23,5 +27,21 @@
             tbody.append(clone);
         }
         
-    } 
-})();
+    }
+    
+    function createUser() {
+        var username = $('#usernameFld').val();
+        var password = $('#passwordFld').val();
+        var firstName = $('#firstNameFld').val();
+        var lastName = $('#lastNameFld').val();
+
+        var user = {
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
+        };
+
+        userService.createUser(user).then(findAllUsers);
+    }
+})(); 
