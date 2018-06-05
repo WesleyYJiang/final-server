60520ce4e3f6dc9599cba99a420e453fdd58f699
wesleyjiang <jiang.yuto@husky.neu.edu>
2018-06-04 21:44:46 -0400

    added exam and question

--- src/main/java/com/example/webdevhw/models/Widget.java
+++ src/main/java/com/example/webdevhw/models/Widget.java
@@ -5,9 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 
 @Entity
+@Inheritance(strategy= InheritanceType.SINGLE_TABLE)
 public class Widget {
   @Id
   @GeneratedValue(strategy= GenerationType.IDENTITY)
