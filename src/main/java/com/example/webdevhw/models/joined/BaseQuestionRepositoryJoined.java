c025f9ea23269fedb4d5197551b7b61d2f6e75d3
wesleyjiang <jiang.yuto@husky.neu.edu>
2018-06-04 22:15:26 -0400

    joined questions

---
+++ src/main/java/com/example/webdevhw/models/joined/BaseQuestionRepositoryJoined.java
@@ -0,0 +1,8 @@
+package com.example.webdevhw.models.joined;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BaseQuestionRepositoryJoined
+        extends CrudRepository<BaseQuestionJoined, Integer>{
+}
